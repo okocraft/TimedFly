9f8bc2a814d5204b4931328d419ec13b59789937
Siroshun09 <siro.shun8@gmail.com>
2020-07-14 07:00:19 +0900

    [Add] 1.16.1 nms

---
+++ src/main/java/me/jackint0sh/timedfly/versions/v1_16/v1_16_R1.java
@@ -0,0 +1,37 @@
+package me.jackint0sh.timedfly.versions.v1_16;
+
+import me.jackint0sh.timedfly.versions.ServerVersion;
+import net.minecraft.server.v1_16_R1.ItemStack;
+import net.minecraft.server.v1_16_R1.NBTTagCompound;
+import org.bukkit.craftbukkit.v1_16_R1.inventory.CraftItemStack;
+
+public class v1_16_R1 extends ServerVersion {
+
+    @Override
+    public org.bukkit.inventory.ItemStack setNBT(org.bukkit.inventory.ItemStack itemStack, String key, String value) {
+        ItemStack stack = CraftItemStack.asNMSCopy(itemStack);
+        NBTTagCompound tag = stack.getTag() != null ? stack.getTag() : new NBTTagCompound();
+
+        tag.setString(key, value);
+        stack.setTag(tag);
+
+        return CraftItemStack.asCraftMirror(stack);
+    }
+
+    @Override
+    public boolean hasTag(org.bukkit.inventory.ItemStack itemStack, String key) {
+        ItemStack stack = CraftItemStack.asNMSCopy(itemStack);
+        NBTTagCompound tag = stack.getTag();
+
+        if (tag == null) return false;
+        return !tag.getString(key).isEmpty();
+    }
+
+    @Override
+    public String getTag(org.bukkit.inventory.ItemStack itemStack, String key) {
+        ItemStack stack = CraftItemStack.asNMSCopy(itemStack);
+        NBTTagCompound tag = stack.getTag();
+        if (tag == null) return null;
+        return tag.getString(key);
+    }
+}
